4208f56728c4486f4c524fb4094af24e99a5c7a4
bolo"n <boguslaw.fraszko@gmail.com"n>
2023-09-11 21:27:55 +0200

    import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol

--- test/fuzz/failOnRevert/StopOnRevertInvariants.t.sol
+++ test/fuzz/failOnRevert/StopOnRevertInvariants.t.sol
@@ -13,7 +13,7 @@ import {DSCEngine} from "../../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../../script/HelperConfig.s.sol";
 import {DeployDSC} from "../../../script/DeployDSC.s.sol";
-import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 import {StopOnRevertHandler} from "./StopOnRevertHandler.t.sol";
 import {console} from "forge-std/console.sol";
 
@@ -44,13 +44,17 @@ contract StopOnRevertInvariants is StdInvariant, Test {
     function setUp() external {
         DeployDSC deployer = new DeployDSC();
         (dsc, dsce, helperConfig) = deployer.run();
-        (ethUsdPriceFeed, btcUsdPriceFeed, weth, wbtc,) = helperConfig.activeNetworkConfig();
+        (ethUsdPriceFeed, btcUsdPriceFeed, weth, wbtc, ) = helperConfig
+            .activeNetworkConfig();
         handler = new StopOnRevertHandler(dsce, dsc);
         targetContract(address(handler));
         // targetContract(address(ethUsdPriceFeed)); Why can't we just do this?
     }
 
-    function invariant_protocolMustHaveMoreValueThatTotalSupplyDollars() public view {
+    function invariant_protocolMustHaveMoreValueThatTotalSupplyDollars()
+        public
+        view
+    {
         uint256 totalSupply = dsc.totalSupply();
         uint256 wethDeposted = ERC20Mock(weth).balanceOf(address(dsce));
         uint256 wbtcDeposited = ERC20Mock(wbtc).balanceOf(address(dsce));
